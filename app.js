070194cc591f8bab9638b26875c327ab38322cb1
realizemyapp <realizemyapp.com@gmail.com>
2018-08-27 21:29:54 +0000

    basic material design

--- app.js
+++ app.js
@@ -19,13 +19,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+// app.use('/', express.static(__dirname + '/app'));
 app.use('/users', usersRouter);
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
